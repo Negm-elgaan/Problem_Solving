c2eaeb3479f991ae707e9871f5a6840d16ea4797
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2026-04-18 00:06:55 +0200

    Create LRUCache.cpp

---
+++ Hackerrank/LRUCache.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <cassert>
+using namespace std;
+
+struct Node{
+   Node* next;
+   Node* prev;
+   int value;
+   int key;
+   Node(Node* p, Node* n, int k, int val):prev(p),next(n),key(k),value(val){};
+   Node(int k, int val):prev(NULL),next(NULL),key(k),value(val){};
+};
+
+class Cache{
+
+   protected: 
+   map<int,Node*> mp; //map the key to the node in the linked list
+   int cp;  //capacity
+   Node* tail; // double linked list tail pointer
+   Node* head; // double linked list head pointer
+   virtual void set(int, int) = 0; //set function
+   virtual int get(int) = 0; //get function
+
+};
+
+class LRUCache : public Cache
+{
+    public:
+
+        LRUCache(int c)
+        {
+            cp = c;
+            tail = nullptr;
+            head = nullptr;
+        }
+
+        void set(int x , int y)
+        {
+            if (mp.find(x) != mp.end())
+            {
+
+                mp[x]->value = y;
+                Node* temp = mp[x];
+                if (temp == tail)
+                {
+                    return;
+                }
+                if (temp == head)
+                {
+                    tail->next = temp;
+                temp->prev = tail;
+                tail = temp;
+                head = temp->next;
+                return;
+                }
+                temp->prev->next = temp->next;
+                temp->next->prev = temp->prev;
+                tail->next = temp;
+                temp->prev = tail;
+                tail = temp;
+            }
+            else
+            {
+                if (mp.size() >= cp)
+                {
+                    Node* temp = head;
+                    head = head->next;
+                    head->prev = nullptr;
+                    mp.erase(temp->key);
+                }
+
+                if (!tail)
+                {
+                    Node* node = new Node(nullptr , nullptr , x , y);
+                    head = node;
+                    tail = node;
+                    mp[x] = node;
+                }
+                else
+                {
+                    Node* node = new Node(tail , nullptr , x , y);
+                    tail->next = node;
+                    tail = node;
+                    mp[x] = node;
+                }
+
+
+            }
+        }
+
+        int get(int x)
+        {
+            if (mp.find(x) != mp.end())
+            {
+                 Node* temp = mp[x];
+                if (temp == tail)
+                {
+                    return mp[x]->value;
+                }
+                if (temp == head)
+                {
+                    tail->next = temp;
+                temp->prev = tail;
+                tail = temp;
+                head = temp->next;
+                return mp[x]->value;
+                }
+                temp->prev->next = temp->next;
+                temp->next->prev = temp->prev;
+                tail->next = temp;
+                temp->prev = tail;
+                tail = temp;
+                return mp[x]->value;
+            }
+
+            return -1;
+        }
+};
+
+int main() {
+   int n, capacity,i;
+   cin >> n >> capacity;
+   LRUCache l(capacity);
+   for(i=0;i<n;i++) {
+      string command;
+      cin >> command;
+      if(command == "get") {
+         int key;
+         cin >> key;
+         cout << l.get(key) << endl;
+      } 
+      else if(command == "set") {
+         int key, value;
+         cin >> key >> value;
+         l.set(key,value);
+      }
+   }
+   return 0;
+}
